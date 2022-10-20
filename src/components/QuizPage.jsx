e8b0847e4837b8640a5fea959c206592059fadf6
stuey-b <stuartwbartlett@gmail.com>
2022-10-20 18:02:46 +0900

    Add quiz page html and basic layout.

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,36 +1,34 @@
-import React from 'react'
-import Card from './Card'
-
+import React from "react";
+import Card from "./Card";
 
 const QuizPage = () => {
   return (
     <Card>
-    <div className="bg-yellow-200 h-1/2 p-8 flex flex-col justify-between items-center font-bold text-8xl rounded-b-3xl">
-      {/* <img
-        className="mx-auto pt-10 drop-shadow-2xl"
-        src="/images/speechbubble1.png"
-        alt=""
-      /> */}
-
-
-      <div className='bg-white w-5/6 h-1/2 mx-auto rounded-3xl flex justify-center items-center'>
-      <h2 className="">る</h2>
+     <div className="flex justify-around items-center h-16">
+        <button>Retry</button>
+        <button>Quit</button>
+        
       </div>
-      
-      <img
-        className="drop-shadow-2xl h-1/2 py-4"
-        src="images/cat1.png"
-        alt="cat1"
-      />
-    </div>
-    <div className="flex flex-col justify-center items-center space-y-6 h-1/2">
-      <div className="Buttons">A</div>
-      <div className="Buttons">Shi</div>
-      <div className="Buttons">Ru</div>
-      <div className="Buttons">He</div>
-    </div>
-  </Card>
-  )
-}
+      <div className="bg-pink-200 h-1/2 p-8 flex flex-col justify-between items-center font-bold text-8xl rounded-3xl">
+        <div className="bg-white w-5/6 h-1/2 mx-auto rounded-3xl flex justify-center items-center">
+          <h2 className="">る</h2>
+        </div>
 
-export default QuizPage
+        <img
+          className="drop-shadow-2xl h-1/2 py-4"
+          src="images/cat1.png"
+          alt="cat1"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center space-y-6">
+        <button className="Buttons">A</button>
+        <button className="Buttons">Shi</button>
+        <button className="Buttons">Ru</button>
+        <button className="Buttons">He</button>
+      </div>
+     
+    </Card>
+  );
+};
+
+export default QuizPage;
